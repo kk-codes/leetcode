dd04cf519b7e267792f390cd1c118a5b522d87d9
kamalesh <101338230+kk-codes@users.noreply.github.com>
2024-03-19 17:18:24 +0530

    Time: 1 ms (52.87%) | Memory: 44.5 MB (42.42%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -11,10 +11,7 @@ class Solution {
         for(int i =0;i<n;i++){
             for(int j=0;j<n;j++){
                 temp[i][j] = image[i][n-j-1];
-                if(temp[i][j]==0)
-                    temp[i][j]=1;
-                else
-                    temp[i][j]=0;
+                temp[i][j] ^=1;
             }
         }
         return temp;
