d1fae3786b86f29273fad87b22ce1860732e8a36
kamalesh <101338230+kk-codes@users.noreply.github.com>
2024-03-19 17:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (15.12%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -7,9 +7,20 @@ class Solution {
         To inverse:
             change 1 to 0 and 0 to 1*/
       int n = image.length;
-      int temp =-1;
-        //int[][] temp = new int[n][n];
+        /*int[][] temp = new int[n][n];
         for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                temp[i][j] = image[i][n-j-1];
+                temp[i][j] ^=1;
+            }
+        }
+        return temp;
+        
+        T.c = O(n^2)
+        S.c = O(n^2);
+        */
+        int temp=-1;
+         for(int i =0;i<n;i++){
             for(int j=0;j<(n+1)/2;j++){
                 temp = image[i][n-j-1];
                 image[i][n-j-1] = image[i][j]^1;
@@ -17,5 +28,8 @@ class Solution {
             }
         }
         return image;
+        //T.C O(n*n/2)
+        //S.C O(1)
     }
+    
 }
